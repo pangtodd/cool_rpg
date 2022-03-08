5950d35fe6da466b99fbb497bcfd645673493426
Todd Pangilinan <pang.todd@gmail.com>
2022-03-07 20:55:03 -0800

    passed test one, creating a test Game object.

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -3,5 +3,9 @@ import Game from '../src/js/game';
 describe('Game', () => {
 
   test('should correctly create a Game object with one attribute', () => {
-    const livingThingOne = new LivingThing("name", 100, 80);
-    expect(livingThingOne.name).toEqual("name");
+    let hero = {name: "hero", health: 100, attackPower: 80};
+    let gameOne = new Game(hero);
+    expect(gameOne.livingThings).toEqual(hero);
+  });
+}
+
